729d5c5aef0941a3de1f6a63d9cee0da412b70b8
David Chou <david.chou@umbocv.com>
2016-05-14 20:37:34 +0800

    AvBitstreamFilterFilter: Remove a string parameter and directly pass NULL to C.av_bitstream_filter_filter()

--- avcodec/bitstreamfilter.go
+++ avcodec/bitstreamfilter.go
@@ -21,8 +21,8 @@ func (f *BitStreamFilter) AvBitstreamFilterNext() *BitStreamFilter {
 }
 
 //Filter bitstream.
-func (bfx *BitStreamFilterContext) AvBitstreamFilterFilter(ctxt *Context, a string, p **uint8, ps *int, b *uint8, bs, k int) int {
-	return int(C.av_bitstream_filter_filter((*C.struct_AVBitStreamFilterContext)(bfx), (*C.struct_AVCodecContext)(ctxt), C.CString(a), (**C.uint8_t)(unsafe.Pointer(p)), (*C.int)(unsafe.Pointer(ps)), (*C.uint8_t)(b), C.int(bs), C.int(k)))
+func (bfx *BitStreamFilterContext) AvBitstreamFilterFilter(ctxt *Context, p **uint8, ps *int, b *uint8, bs, k int) int {
+	return int(C.av_bitstream_filter_filter((*C.struct_AVBitStreamFilterContext)(bfx), (*C.struct_AVCodecContext)(ctxt), (*C.char)(unsafe.Pointer(uintptr(0))), (**C.uint8_t)(unsafe.Pointer(p)), (*C.int)(unsafe.Pointer(ps)), (*C.uint8_t)(b), C.int(bs), C.int(k)))
 }
 
 //Release bitstream filter context.
